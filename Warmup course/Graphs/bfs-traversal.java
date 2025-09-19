3a566bceb87b74242068b6f42959df77b48f57c1
Akshit Yadav <akshit07032001@gmail.com>
2025-09-19 13:16:13 +0530

    level order

--- Warmup course/Graphs/bfs-traversal.java
+++ Warmup course/Graphs/bfs-traversal.java
@@ -8,28 +8,39 @@ public class BFS {
 
         queue.add(start); 
 
-        System.out.print("BFS Traversal: ");
-        while (!queue.isEmpty()) {
-            int popped = queue.poll(); 
-            System.out.print(popped + " ");
+        System.out.println("BFS Traversal: ");
+        Map<Integer, Integer> levels = new HashMap<>();
+        levels.put(start, 0); // Start node is at level 0
 
-            for (int node: graph.get(popped)) {
-                if (!visited[node]) {
-                    queue.add(node); 
-                    visited[node] = true; 
+        while (!queue.isEmpty()) {
+            int size = queue.size(); // Number of nodes at this level
+            // process all nodes at the current level
+            for (int i = 0; i < size; i++) {
+                int popped = queue.poll(); 
+                int level = levels.get(popped);
+
+                System.out.println("Level " + level + ": " + popped);
+
+                for (int node : graph.get(popped)) {
+                    if (!visited[node]) {
+                        queue.add(node); 
+                        visited[node] = true; 
+                        levels.put(node, level + 1); // child's level = parent level + 1
+                    }
                 }
             }
         }
     }
+
     static void addEdge(List<List<Integer>> graph, int v, int u) {
         graph.get(u).add(v);
         graph.get(v).add(u);
     }
+
     public static void main(String[] args) {
         int n = 6; 
-
         List<List<Integer>> graph = new ArrayList<>(); 
-        for (int i=0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<>());
         }
 
